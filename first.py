2cf1af22414db86bab15e5fa37db5b1758994ecb
mjoelnir97 <gomekaratharva@gmail.com>
2017-03-18 18:54:54 +0530

    Update first.py
    suggested by mayank

--- first.py
+++ first.py
@@ -9,21 +9,9 @@ reg1 = re.compile('SGPA')
 count = 10 
 
 dic ={};
-# for line in file:
-	
-# 	count +=1  
-# 	if(reg1.match(line)):
-# 		count =0 
-
-# 	if(count ==2):
-# 		sgpa.append(line)
-# 		count = 10 
-			
-# 	if(reg.match(line)):
-# 		roll += [line]
 rol=0;
 for line in file:
-	# count +=1  
+	
 	if(reg1.match(line)):
 		line=file.next();
 		line=file.next();
@@ -32,30 +20,10 @@ for line in file:
 	if(reg.match(line)):
 		rol=line;
 		dic[rol]=0;
-
-
-
-
-
-
-
-
-
-
-
-
+		
 file.close()
-# print roll
-# print sgpa
-
-
-# dictionary  = dict(zip(roll, sgpa))
-# # print(dictionary)
-
-# sort_dict = sorted(dictionary.items(), key = itemgetter(1))
 
 sort_dict=sorted(dic.items(),key=itemgetter(1))
-
 print (sort_dict)
 
 
